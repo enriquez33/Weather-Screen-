45ad8d37e03892876083161d4e0c60d89286eb77
preston <pjenriquez1994@gmail.com>
2024-02-16 10:20:51 -0600

    Initial Commit

---
+++ Screen/ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenApp.swift
+//  Screen
+//
+//  Created by Preston Enriquez on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
